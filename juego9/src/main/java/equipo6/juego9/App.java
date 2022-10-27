b2b39697732d9baf99ff6a7684b144f3572653b8
Victoria Geeks ♡ <victoriageeks@gmail.com>
2022-10-27 12:08:40 +0200

    primer commit

---
+++ juego9/src/main/java/equipo6/juego9/App.java
@@ -0,0 +1,13 @@
+package equipo6.juego9;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
